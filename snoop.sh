f148d0fadd18b203fd7a943cb003cc1b3d1090d8
Dhruv Batheja <dhruv.batheja@booking.com>
2019-12-16 16:02:21 +0100

    Updating executable

--- snoop.sh
+++ snoop.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/live-wire/snooper/master/x/snooper --output ~/.snooper && . ~/.snooper && echo "[FIN]"
+curl https://raw.githubusercontent.com/live-wire/snooper/master/x/snooper --output ~/.snooper && ~/.snooper && 
+echo "[FIN]"
