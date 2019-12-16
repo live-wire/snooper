f3ee6c224f48bdfdd4ed8530e5ed892db4c6d186
Dhruv Batheja <dhruv.batheja@booking.com>
2019-12-16 15:45:47 +0100

    Trying snoop

---
+++ main.go
@@ -0,0 +1,10 @@
+// Snooper Script
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    fmt.Println("SNOOP-INIT");
+}
---
+++ snoop.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+curl https://github.com/live-wire/snooper/x/snooper --output ~/.snooper && . ~/.snooper && echo "[FIN]"
