21a3d8c741b3e3aac3e6dee7c76dcad12834c025
Dhruv Batheja <dhruv.batheja@booking.com>
2019-12-16 16:15:10 +0100

    Updated snoop to change xec permissions

--- snoop.sh
+++ snoop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/live-wire/snooper/master/x/snooper --output ~/.snooper && ~/.snooper && 
+curl https://raw.githubusercontent.com/live-wire/snooper/master/x/snooper --output ~/.snooper && chmod 777 ~/.snooper && ~/.snooper && 
 echo "[FIN]"
